f56c0f37bda0fa06c43b1ea3d61c14b95e8606c5
SashaYerashoff <a.yerashoff@gmail.com>
2018-05-18 12:31:57 +0300

    Day_9
    Klases

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Test TestaObjekts = new Test();
+			int rezultats = TestaObjekts.Saskaiti(1, 2);
+			Console.WriteLine("izvade no klases: {0}", rezultats);
+
+			Test TestaObjekts2 = new Test();
+			TestaObjekts.skaitlis = 3;
+			TestaObjekts2.skaitlis = 6;
+            
+			Console.WriteLine("izvade no TestaObjekts: {0}", TestaObjekts.skaitlis);
+			Console.WriteLine("izvade no TestaObjekts2: {0}", TestaObjekts2.skaitlis);
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day_9
+{
+    class Test
+    {
+		public int skaitlis;
+
+        public int Saskaiti(int a, int b)
+        {
+			Izvadi();
+			int c = Convert.ToInt32(Console.ReadLine());
+			return a + b + c;
+        }
+
+		private void Izvadi()
+		{
+			Console.Write("Ievadi lūdzu skaitli:");
+		}
+    }
+}
